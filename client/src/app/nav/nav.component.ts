7be328d70840799fb59d1fd4ab8d2840f58a40a8
vi8249 <vi8249@gmail.com>
2021-05-07 10:05:28 +0800

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit {
   //loggedIn: boolean;
   currentUser$: Observable<User>;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     //this.getCurrentUser();
@@ -23,15 +25,18 @@ export class NavComponent implements OnInit {
   login() {
     console.log(this.model);
     this.accountService.login(this.model).subscribe(res => { // observable needs to be subscribe
-      console.log(res);
+      //console.log(res);
+      this.router.navigateByUrl("/members");
       //this.loggedIn = true;
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
     //this.loggedIn = false;
   }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -21,8 +22,9 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(res => {
       console.log(res);
       this.cancel();
-    }, error => {
+    }, error => { // http error response
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
